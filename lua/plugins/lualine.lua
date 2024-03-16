87213b24f62836d598b50c77746b11c18b609445
mich-murphy <github@elmurphy.com>
2024-03-16 23:21:05 +1100

    adding lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,71 @@
+return {
+  { -- Customised statusline
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = ' '
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
+    end,
+    opts = function()
+      -- PERF: we don't need this lualine require madness 🤷
+      local lualine_require = require 'lualine_require'
+      lualine_require.require = require
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+
+      return {
+        options = {
+          theme = 'auto',
+          section_separators = '',
+          component_separators = '',
+          globalstatus = true,
+          disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'starter' } },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch' },
+
+          lualine_c = { 'filename' },
+          lualine_x = {
+            'filetype',
+            {
+              require('lazy.status').updates,
+              cond = require('lazy.status').has_updates,
+              color = 'yellow',
+            },
+            {
+              'diff',
+              symbols = {
+                added = ' ',
+                modified = ' ',
+                removed = ' ',
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_y = {
+            { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+            { 'location', padding = { left = 0, right = 1 } },
+          },
+          lualine_z = { 'encoding' },
+        },
+        extensions = { 'neo-tree', 'lazy' },
+      }
+    end,
+  },
+}
