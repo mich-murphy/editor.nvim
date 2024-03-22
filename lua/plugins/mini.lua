b60e44c199cb5b1a75078003535b78d23cc94987
mich-murphy <github@elmurphy.com>
2024-03-22 21:26:27 +1100

    aligning keymaps between mini.surround and flash

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,17 @@
 return {
   { -- Add/delete/replace surroundings (brackets, quotes, etc.)
     'echasnovski/mini.surround',
-    opts = {},
+    opts = {
+      mappings = {
+        add = 'csa', -- Add surrounding in Normal and Visual modes
+        delete = 'csd', -- Delete surrounding
+        find = 'csf', -- Find surrounding (to the right)
+        find_left = 'csF', -- Find surrounding (to the left)
+        highlight = 'csh', -- Highlight surrounding
+        replace = 'csr', -- Replace surrounding
+        update_n_lines = 'csn', -- Update `n_lines`
+      },
+    },
   },
 
   { -- Pair opening bracket etc. with closing
@@ -17,26 +27,6 @@ return {
       symbol = '│',
       options = { try_as_border = true },
     },
-    init = function()
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = {
-          'help',
-          'alpha',
-          'dashboard',
-          'neo-tree',
-          'Trouble',
-          'trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
   },
 
   { -- Comments
